feff1af16d72bbebd97e1725dee870fbf99a2d20
Jeffrey Fisher <jeff@slofish.io>
2019-07-09 15:31:28 -0700

    Fix GitHub pjax issues.
    Closes #3, #4

--- background.js
+++ background.js
@@ -2,16 +2,27 @@
 
 chrome.webRequest.onBeforeRequest.addListener(function (details) {
 	if (details.method === 'GET') {
+		const isOpenQuery = 'q=is:open';
+		const pjaxQueryStart = details.url.search(/\?_pjax=%23js-repo-pjax-container/);
+
+		let newUrl;
+		if (pjaxQueryStart !== -1) {
+			newUrl = details.url.substr(0, pjaxQueryStart + 1) + isOpenQuery;
+		} else {
+			newUrl = details.url + '?' + isOpenQuery;
+		}
+			
 		return {
-			redirectUrl: details.url + '?q=is:open'
+			redirectUrl: newUrl
 		};
 	}
 }, {
 	urls: [
-		'https://github.com/*/*/issues'
+		'https://github.com/*/*/issues',
+		'https://github.com/*/*/issues?_pjax=%23js-repo-pjax-container',
 	],
 	types: [
-		'main_frame'
+		"main_frame", "xmlhttprequest"
 	]
 }, [
 	'blocking'
